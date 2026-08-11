8a1df9275c577865f33b2a0f1659c09b7c2097c3
Yan Zhao <152412597+Zarky2001@users.noreply.github.com>
2026-08-11 08:48:10 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="site-footer">
-            <div className="footer-left">© {new Date().getFullYear()} Zarky (赵妍)</div>
+            <div className="footer-left">© {new Date().getFullYear()} Zarky (赵妍) Follow github project <a>https://long-ling.com</a></div>
         </footer>
     );
 }
